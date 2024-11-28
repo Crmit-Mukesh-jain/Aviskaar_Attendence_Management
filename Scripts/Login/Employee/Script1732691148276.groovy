d5cb15ee422ea096823b6fad9a858ff8836c6509
Crmit-Mukesh-jain <mukesh.jain@crmit.com>
2024-11-28 18:18:43 +0530

    few Changes on 28th

--- Scripts/Login/Employee/Script1732691148276.groovy
+++ Scripts/Login/Employee/Script1732691148276.groovy
@@ -17,3 +17,53 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl(GlobalVariable.URL)
+
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_Refresh_2050'), GlobalVariable.Username)
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login/input_Username_2180'), GlobalVariable.Password)
+
+WebUI.click(findTestObject('Object Repository/Page_Login/span_Log in'))
+
+WebUI.click(findTestObject('Object Repository/Page_My Settings/a_My Leave'))
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/input__Start Date'))
+
+WebUI.setText(findTestObject('Page_ApplyLeave/input__Start Date'), Start_Date)
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/input__End Date'))
+
+WebUI.setText(findTestObject('Page_ApplyLeave/input__End Date'), End_Date)
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/button_None'))
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/span_PrivilegeAnnual Leave'))
+
+WebUI.setText(findTestObject('Object Repository/Page_ApplyLeave/input_Reason_Reason'), Reason)
+
+WebUI.setText(findTestObject('Object Repository/Page_ApplyLeave/input_Inform_input-105'), Inform)
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/button_Apply'))
+
+WebUI.click(findTestObject('Object Repository/Page_ApplyLeave/button_Confirm'))
+
+WebUI.click(findTestObject('Employee All Leave Object'))
+
+WebUI.click(findTestObject('Object Repository/Page_Employee Leave/a_SortEmployee Leave Name'))
+
+WebUI.click(findTestObject('Object Repository/Get_Web_First_Row_ID'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Employee Logout'))
+
+WebUI.click(findTestObject('Logout button'))
+
+WebUI.delay(1)
+
+WebUI.closeBrowser()
+
--- Scripts/Login/Manager/Script1732691499830.groovy
+++ Scripts/Login/Manager/Script1732691499830.groovy
@@ -17,3 +17,59 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl(GlobalVariable.URL)
+
+WebUI.setText(findTestObject('Page_Login/input_Refresh_2050'), GlobalVariable.Username)
+
+WebUI.setEncryptedText(findTestObject('Page_Login/input_Username_2180'), GlobalVariable.Password)
+
+WebUI.click(findTestObject('Page_Login/span_Log in'))
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Manager All Leave Object'))
+
+WebUI.click(findTestObject('Page_Employee Leave/svg_My Applied Leave_slds-icon slds-icon-te_a4c030'))
+
+WebUI.click(findTestObject('Pending Approval Object'))
+
+WebUI.click(findTestObject('Page_Employee Leave/a_SortEmployee Leave Name'))
+
+WebUI.click(findTestObject('Get_Manager_First_Row_Id'))
+
+WebUI.click(findTestObject('Object Repository/Page_Employee Leave EL-4320/button_Reject'))
+
+WebUI.setText(findTestObject('Reject Text Message'), 'No Leaves Only Work')
+
+WebUI.click(findTestObject('Object Repository/Page_Employee Leave EL-4320/button_OK'))
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Manager All Leave Object'))
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Object Repository/Page_Employee Leave/svg_My Applied Leave_slds-icon slds-icon-te_a4c030'))
+
+WebUI.click(findTestObject('All Rejected List View'))
+
+WebUI.click(findTestObject('Page_Employee Leave/a_SortEmployee Leave Name'))
+
+WebUI.click(findTestObject('Get_Manager_First_Row_Id'))
+
+WebUI.verifyElementText(findTestObject('Status Rejected'), 'Rejected')
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Logout Object'))
+
+WebUI.click(findTestObject('Logout button'))
+
+WebUI.delay(1)
+
+WebUI.closeBrowser()
+
